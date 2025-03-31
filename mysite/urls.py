dd7985670f0ba93705de0426660da1a6dba415e5
Vaucfra99 <victorau93@gmail.com>
2025-03-31 23:56:23 +0200

    Add comments

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,6 +5,6 @@ from django.contrib.auth import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    path('accounts/login/', views.auth_login, name = 'login'),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
     path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
